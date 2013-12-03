951f56948b654c8b2548375f2fe2a85d7738254c
Johnny <johnny@ubuntu.ubuntu-domain>
2013-12-03 21:01:52 +0100

    L-system

---
+++ L-system.pas
@@ -0,0 +1,108 @@
+program LSystem;
+
+const firstASCII = chr(0);		{pierwszy i ostatni "rozsadny" znak w tablicy ASCII} 
+      lastASCII = chr(127);
+
+type AsciiArray = array[firstASCII..lastASCII] of string;
+
+var i : char;
+    A : AsciiArray;
+    axiom : ansistring;
+    repeats,j : longint;
+
+procedure description(var A : AsciiArray);
+
+  var s : string;
+      enter : boolean;
+
+  begin 
+
+    enter := false;
+
+    while (not enter) do begin
+      readln(s);
+      if length(s) <> 0 then A[s[1]] := s
+      else enter := true;
+    end;
+
+  end;
+
+procedure translate(var A : ASCIIArray; var axiom : ansistring);
+
+  var i,j : longint;  
+      newAxiom, word : ansistring;
+
+  begin
+   
+    newAxiom := '';  
+    word := '';
+    for i := 1 to length(axiom) do begin
+      word := A[axiom[i]];
+      for j := 2 to length(word) do begin
+        newAxiom := newAxiom + word[j]
+      end;
+    end;
+    axiom := newAxiom;
+
+  end;
+
+procedure copyPaste;
+
+  var s : string;
+      enter : boolean;
+ 
+  begin
+    
+    enter := false;
+
+    while (not enter) do begin
+      readln(s);
+      if length(s) <> 0 then writeln(s)
+      else enter := true;
+    end;
+
+  end;
+
+procedure interpret(var A :ASCIIArray; var axiom : ansistring);
+
+  var i,j : longint;
+      newAxiom, word : ansistring;
+
+  begin
+
+    word := '';
+    for i := 1 to length(axiom) do begin
+      word := A[axiom[i]];
+      newAxiom := '';
+      for j := 2 to length(word) do begin
+        newAxiom := newAxiom + word[j];
+      end;
+      if length(word) > 0 then
+        writeln(newAxiom);
+    end;
+   
+
+  end;
+
+begin
+
+  for i := firstASCII to lastASCII do
+    A[i] := i+i;
+  
+  readln(repeats);
+  readln(axiom);
+  description(A);
+  for j := 1 to repeats do
+    translate(A,axiom);
+  
+  copyPaste;
+
+  for i :=firstASCII to lastASCII do
+    A[i] := '';
+
+  description(A);
+  interpret(A,axiom); 
+
+  copyPaste;
+
+end.
