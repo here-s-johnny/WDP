f774a6507654ea439961cd777bb650866e93ec17
johnny-the-strange <gajl.jan@gmail.com>
2014-01-21 21:00:39 +0100

    hopefully almost done

--- rectangles.pas
+++ rectangles.pas
@@ -15,19 +15,15 @@ type bigList = ^elemOfBigList;
        last : list;
     end; 
 
-var L : bigList; 
+var L,K : bigList; 
+    startX,startY,length,width,i : integer;
 
 procedure initBigList(L : bigList);
-
-  var pom : bigList;
-
+ 
   begin
-    new(pom);
-    pom^.next := l;
-    pom^.y := 0;
-    pom^.l := nil;
-    pom^.last := pom;
-    L := pom;
+    L^.next := nil;
+    L^.y := 0;
+    L^.l := nil;
   end;
 
 procedure addToBigList(var L : bigList; o : integer; z : integer);
@@ -43,40 +39,40 @@ procedure addToBigList(var L : bigList; o : integer; z : integer);
       L^.y := z;
       L^.l := nil;
     end else begin
+      while L^.next <> nil do L := L^.next;
       new(pom);
       pom^.next := nil;
       pom^.y := z;
       pom^.l := nil;
-      L^.last^.next := pom;
-      L^.last := pom;
+      L^.next := pom;
     end;
   end;  
 
 procedure addToList(var l : list; o :integer; z : integer);
 
-  var pom : list;
+  var pom, pom2 : list;
 
   begin
     if o = 0 then begin
       pom := l;
       new(l);
       l^.next := pom;
-      l^.last := pom^.last;
       l^.x := z;
       l^.c := ' ';
     end else if o = 1 then begin
+      pom2 := l;
+      while l^.next <> nil do l := l^.next;
       new(pom);
       pom^.next := nil;
       pom^.x := z;
       pom^.c := ' ';
-      l^.last^.next := pom;
-      l^.last := pom;
+      l^.next := pom;
+      l := pom2;
     end else begin
       new(l);
       l^.next := nil;
-      l^.last := l;
       l^.x := z;
-      l^.c := '#';
+      l^.c := ' ';
     end;  
   end;
 
@@ -93,24 +89,140 @@ procedure printStructure(L : bigList);
     end;
   end;   
 
-procedure getLine(var startX, startY, length, width : integer);
+procedure getLine(var startX, startY, length, width : integer; first : boolean);
 
   begin
-    while not eoln do begin
-      read(startX);
-      read(startY)
+    if not eoln then begin
+      if first then begin
+        while not eoln do begin
+          startX := 0;
+          startY := 0;
+          read(length);
+          read(width);
+        end;
+      end else begin
+        while not eoln do begin
+          read(startX);
+          read(startY);
+          read(length);
+          read(width);
+        end;
+      end;
+      readln;
+    end;
+  end;
+    
+procedure goToStartPoint(var L : bigList; var K : bigList; y,width: integer);
+
+  var i,j : integer;  
+      pom : bigList;
+  begin
+    pom := L;
+    if y < L^.y then begin
+      for i := L^.y -1 downto y do 
+        addToBigList(L,0,i);
+      K := L;
+      j := L^.y;
+      while L^.next <> nil do L := L^.next;
+      for i := L^.y + 1 to y + width - L^.y - j do addToBigList(L,1,i);
+      L := K;
+    end else if y > L^.y then begin
+      j := L^.y;
+      while L^.next <> nil do L := L^.next;
+      for i := L^.y + 1 to y + width - L^.y - j do addToBigList(L,1,i);
+      K := L;
+      j := L^.y;
+      while L^.next <> nil do L := L^.next;
+      for i:= L^.y + 1 to y + width - L^.y - j do addToBigList(L,1,i);
+      L := pom;
+    end else begin
+      K := L;
+      j := L^.y;
+      while L^.next <> nil do L := L^.next;
+      for i:= L^.y + 1 to y + width - L^.y - j do addToBigList(L,1,i);
+      L := pom;
+    end;
+  end;
+
+procedure expandList(L : bigList; x,length : integer);
+
+  var i : integer;
+
+  begin
+    while L <> nil do begin
+      if L^.l = nil then begin 
+        addToList(L^.l,2,0);
+        for i := x+1 to x+length-1 do addToList(L^.l,1,i); 
+      end else if L^.l^.x > x then begin
+        if length-abs(x) > 0 then
+          for i := L^.l^.x + 1 to length-abs(x) do addToList(L^.l,1,i);
+        for i := L^.l^.x -1 downto x do addToList(L^.l,0,i)
+      end else if L^.l^.x < x then begin
+        while L^.l^.x < x do begin
+          if L^.l^.next = nil then addToList(L^.l,1,L^.l^.x +1) 
+          else L^.l := L^.l^.next;
+        end;
+        for i := x+1 to length do addToList(L^.l,1,i);
+      end else 
+        for i := x+1 to length do addToList(L^.l,1,i);
+      L := L^.next;
+    end;
+  end;   
+
+procedure hash(K : bigList; x,length : integer);
  
+  var i : integer;
+
+  begin
+    while K^.l^.x < x do K^.l := K^.l^.next;
+    for i := x to length -1 do begin
+      writeln(K^.y);
+      writeln(i);
+      K^.l^.c := '#';
+      writeln(K^.l^.c);
+      K^.l := K^.l^.next;
+    end;
+    //K^.l^.c := '#';
+  end;
+
 
 begin
 
 new(L);
-initbigList(L);
-addToList(L^.l,2,0);
-addToList(L^.l,1,1);
-addToList(L^.l,1,2);
-addToList(L^.l,0,3);
+initBigList(L);
+new(K);
 
-printStructure(L);
+{writeln(L^.l^.c, L^.l^.x);
+addToList(L^.l,0,-1);
+addToList(L^.l,1,1);
+addToList(L^.l,0,-2);
+while L^.l <> nil do begin
+  writeln(L^.l^.c, L^.l^.x);
+  L^.l := L^.l^.next;
+end;}
+
+  getLine(startX,startY,length,width,true);
+    goToStartPoint(L,K,startY,width);
+    expandList(L,startX,length);
+    for i := 1 to width do begin
+      hash(K,startX,length);  
+      K := K^.next;
+    end;
+ while L <> nil do begin
+   writeln(L^.l^.c);
+   L := L^.next;
+end;
+  {while not eoln do begin    
+    getLine(startX,startY,length,width,false);
+    writeln(startX);
+    writeln(startY);
+    writeln(length);
+    writeln(width);
+    goToStartPoint(L,K,startY,width);
+    expandList(L,startX,length);
+    hash(K,startX,length);  
+end;}
+//printStructure(L);
 end.
 
 
